5814e66ef5aee0c830bf289cfc14db7c37d130ac
“2018011852” <993951053@qq.com>
2020-10-23 09:00:58 +0800

    淘宝页面

---
+++ build/precache-manifest.c17c28d546bdd1ea20a153a9eec00589.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5691d088846b57882a7dbedaef7933",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b7acd0e7cc5b4e1dd4d9",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "b7acd0e7cc5b4e1dd4d9",
+    "url": "./static/js/2.d3360e30.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "./static/js/2.d3360e30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062e126f168e1b4d265a",
+    "url": "./static/js/main.12c86c88.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  },
+  {
+    "revision": "11e5ba8aa22c8518afc66a0b52361d45",
+    "url": "./static/media/1.11e5ba8a.jpg"
+  },
+  {
+    "revision": "6a0b0a0a06f06601b20fa22b375cde52",
+    "url": "./static/media/2.6a0b0a0a.jpg"
+  },
+  {
+    "revision": "92edc9fc42c8ec0d05a6b5567d93adee",
+    "url": "./static/media/3.92edc9fc.jpg"
+  },
+  {
+    "revision": "35eb894e041d53e540792f23060c0f2b",
+    "url": "./static/media/4.35eb894e.jpg"
+  },
+  {
+    "revision": "6608fe949ac47882ba92a7e9376f5667",
+    "url": "./static/media/5.6608fe94.jpg"
+  },
+  {
+    "revision": "e83a9eea3f7e9c3463874d34f1c376e7",
+    "url": "./static/media/detail1.e83a9eea.jpg"
+  },
+  {
+    "revision": "29091c7cb2add87893ba568c32ad0c66",
+    "url": "./static/media/detail2.29091c7c.jpg"
+  }
+]);
